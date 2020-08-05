2a742366826a02a0937b6ac29df3ee4457bb7c5c
Arkadeep De <arkadeepde142@gmail.com>
2020-08-05 22:12:19 +0530

    Basic Design

---
+++ docs/js/main.js
@@ -0,0 +1,4 @@
+// const callToActionButton = document.querySelector(".call-to-action")
+// callToActionButton.addEventListener("click", (event) => {
+//     callToActionButton.classList.toggle("")
+// })
