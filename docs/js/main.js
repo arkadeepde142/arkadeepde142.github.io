a1a72463aadc053212ec2dbca00d2dc4dd10d77d
Arkadeep De <arkadeepde142@gmail.com>
2020-08-19 19:24:02 +0530

    Further Modifications

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,3 +2,18 @@
 // callToActionButton.addEventListener("click", (event) => {
 //     callToActionButton.classList.toggle("")
 // })
+
+
+const goToTopButton = document.getElementById("go-to-top");
+window.addEventListener('scroll', (e)=> {
+    if(document.body.scrollTop > window.innerHeight/10 || document.documentElement.scrollTop > window.innerHeight / 10) {
+        goToTopButton.hidden = false;
+    } else {
+        goToTopButton.hidden = true;
+    }
+})
+goToTopButton.addEventListener('click', (e) => {
+    if(!goToTopButton.hidden) {
+        document.querySelector("body").scrollIntoView({behavior: "smooth"});
+    }
+})
